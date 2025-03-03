506d480d8f14801787779c17a4f4e86bc4fca916
harshang patel <113461980+harshangpate@users.noreply.github.com>
2025-03-03 09:57:00 +0000

    learned about Js Datatypes

---
+++ 01_basics/02_Datatypes.js
@@ -0,0 +1,32 @@
+"use strict"; // treat all JS code as newer version
+
+//alert(3 + 3) we are using nodejs, not browser
+
+// code redability should be high 
+
+console.log(3+3)
+
+console.log("Harshang"); 
+
+
+let name = "harshang"
+let age = 18
+let isLoggesIn = true
+
+
+// number => 2 to power 52
+// bigint =>  used in the large industries/ websites to handle the large load.
+// string => "" 
+// boolean => true/false
+// null => standalone value
+    //example
+    let null_1 = null;
+//undefined => this is when we have not defined any value
+    //example 
+    let undef_1;
+// symbol => uniqueness
+
+//object 
+
+console.log(typeof undefined); // output: undefined
+console.log(typeof null); // output: object
