8c7c1de380e4e41d60dd7c2f627632bba7185dc4
harshang patel <113461980+harshangpate@users.noreply.github.com>
2025-03-06 08:40:05 +0000

    discussed about object in Js

---
+++ 02_basics/04_objects_p2.js
@@ -0,0 +1,54 @@
+// singolton
+// const facebookUser = new Object()        // singolton object
+const facebookUser = {}                     // non-singolton object
+
+facebookUser.id = "123abc"
+facebookUser.name = "Happy"
+facebookUser.isLoggedIn = false
+
+// console.log(facebookUser)
+
+const googleUser = {
+    email: "Happy@gmail.com",
+    fullname: {
+        userFullname: {
+            firstname: "Happy",
+            lastname: "Patel"
+        }
+    }
+}
+// console.log(googleUser.fullname?.userFullname.lastname);
+// use question mark if we dont have that value in the object for extra protection
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "a", 4: "b"}
+const obj4 = {5: "a", 6: "b"}
+
+// const obj3 ={obj1,obj2}
+// const obj3 = Object.assign({}, obj1, obj2, obj4)       // {} is an optional but it's a good practice to give it. 
+
+const obj3 = {...obj1, ...obj2, ...obj4}
+// console.log(obj3);
+
+// specially used database kind of word
+const users = [
+    {
+        id : 1,
+        email: "Happy@gmail.com"
+    }, {
+        id : 2,
+        email: "Happy@gmail.com"
+    }, {
+        id : 3,
+        email: "Happy@gmail.com"
+    },
+]
+
+users[1].email
+console.log(facebookUser);
+
+console.log(Object.keys(facebookUser));     //to print all the keys
+console.log(Object.values(facebookUser));   // to print all the values 
+console.log(Object.entries(facebookUser));  //comperatively used less
+
+console.log(facebookUser.hasOwnProperty('isLoggedIn')) // to see if this key is present in our object
