19871e61036e785ef12fed5bb3503118cefd02c1
harshang patel <113461980+harshangpate@users.noreply.github.com>
2025-03-07 10:26:24 +0000

    discussed about functions with objects and arrays

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -36,5 +36,38 @@ function loginUserMessage(username /* was can also predefine the calue of userna
     }
     return `${username} just logged in`
 }
-console.log(loginUserMessage());  // undefined
+// console.log(loginUserMessage());  // undefined
 // console.log(loginUserMessage("Happy"));
+
+
+/* ******************* Shopping cart example ********************* */
+
+function calculateCartPrice(...num1){
+    return num1
+}
+// console.log(calculateCartPrice(200, 400, 500));
+// ... is also called as splash and rest operator used to combine all the values in one array
+
+
+const user = {
+    username: "Happy",
+    price: 200
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+    
+}
+// handleObject(user)
+// handleObject({
+//     username: "sam",
+//     price: 399
+// })
+
+const myNewArray = [200, 600, 500, 600]
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+console.log(returnSecondValue(myNewArray)); 
+console.log(returnSecondValue([200, 400, 500, 100]));   //can be also written as this
+
